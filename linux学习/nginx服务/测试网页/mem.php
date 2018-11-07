35388bc571f845421aeeb68f442ff55fb6aaf6df
root <root@room9pc01.tedu.cn>
2018-11-07 16:49:12 +0800

    gai

--- linux学习/nginx服务/测试网页/mem.php
+++
@@ -1,8 +0,0 @@
-<?php
-$memcache=new Memcache;
-$memcache->connect('localhost',11211) or die ('could not connect!! ');
-$memcache->set('key', 'test');
-$get_values=$memcache->get('key');
-echo $get_values;
-?>
-
